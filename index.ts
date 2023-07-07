df1b457a673071e2d3f81febf72c2e87b0f58639
Gabriel Harel <gabrielj.harel@gmail.com>
2023-07-06 23:06:57 -0500

    write wireframes to readme

--- index.ts
+++ index.ts
@@ -1,5 +1,15 @@
-import { glob } from 'npm:glob@10.3.1';
+import { glob } from "npm:glob@10.3.1";
 
-const wireframes = await glob("**/wireframe.png");
-console.log(wireframes);
+const wireframes: string[] = await glob("**/wireframe.png");
 
+const readme = `
+# Project Plans
+
+${wireframes.map((img) => `<img src="${img}" width="30%"/>`).join("\n")}
+
+## LICENSE
+
+[MIT](./LICENSE)
+`.trim() + "\n";
+
+await Deno.writeTextFile("./README.md", readme);
