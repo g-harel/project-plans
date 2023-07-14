8a86cfd63e3887081139ed04ea825e7c4b307fb5
Gabriel Harel <gabrielj.harel@gmail.com>
2023-07-13 22:12:07 -0500

    slurp projects

--- README.md
+++ README.md
@@ -1,29 +1,29 @@
 # Project Plans
 
 <p align="center">
-  <a href="./plans/suizan-hook">
-    <img src="plans/suizan-hook/wireframe.png" width="45%"/>
-  </a>
-  <a href="./plans/euro-bench">
-    <img src="plans/euro-bench/wireframe.png" width="45%"/>
-  </a>
-  <a href="./plans/scrap-cart">
-    <img src="plans/scrap-cart/wireframe.png" width="45%"/>
-  </a>
-  <a href="./plans/knew-hook">
-    <img src="plans/knew-hook/images/wireframe.png" width="45%"/>
-  </a>
-  <a href="./plans/cross-lap-jig">
-    <img src="plans/cross-lap-jig/images/wireframe.png" width="45%"/>
-  </a>
   <a href="./plans/board-hook">
     <img src="plans/board-hook/images/wireframe.png" width="45%"/>
   </a>
   <a href="./plans/clamping-square">
     <img src="plans/clamping-square/images/wireframe.png" width="45%"/>
   </a>
+  <a href="./plans/cross-lap-jig">
+    <img src="plans/cross-lap-jig/images/wireframe.png" width="45%"/>
+  </a>
   <a href="./plans/dog-home">
     <img src="plans/dog-home/images/wireframe.png" width="45%"/>
+  </a>
+  <a href="./plans/euro-bench">
+    <img src="plans/euro-bench/wireframe.png" width="45%"/>
+  </a>
+  <a href="./plans/knew-hook">
+    <img src="plans/knew-hook/images/wireframe.png" width="45%"/>
+  </a>
+  <a href="./plans/scrap-cart">
+    <img src="plans/scrap-cart/wireframe.png" width="45%"/>
+  </a>
+  <a href="./plans/suizan-hook">
+    <img src="plans/suizan-hook/wireframe.png" width="45%"/>
   </a>
 </p>
 
--- index.ts
+++ index.ts
@@ -1,43 +1,87 @@
 import { glob } from "npm:glob@10.3.1";
 import { dirname } from "https://deno.land/std/path/mod.ts";
-import {
-  ImageMagick,
-  IMagickImage,
-  initializeImageMagick,
-} from "https://deno.land/x/imagemagick_deno@0.0.24/mod.ts";
+// import {
+//   ImageMagick,
+//   IMagickImage,
+//   initializeImageMagick,
+// } from "https://deno.land/x/imagemagick_deno@0.0.24/mod.ts";
 
-await initializeImageMagick();
+// await initializeImageMagick();
 
 const wireframes: string[] = await glob("**/wireframe.png");
 
-const getRootFromWireframe = (path: string) => {
-  while (path.endsWith(".png") || path.endsWith("images")) {
-    path = dirname(path);
+interface Project {
+  name: string;
+  pitch: string;
+  description: string[];
+  wireframe: string;
+}
+
+const getProjects = async (): Promise<Project[]> => {
+  const projects: Project[] = [];
+  for await (const f of Deno.readDir("plans")) {
+    if (f.isDirectory) {
+      projects.push({
+        name: f.name,
+        pitch: "",
+        description: [],
+        wireframe: await glob(`**/${f.name}/**/wireframe.png`).then((r: string[]) => r ? r[0] : ""),
+      });
+    }
   }
-  return path;
+  return projects.sort((a, b) => a.name < b.name ? -1 : 1);
 };
 
 const getWidth = async (img: string): Promise<number> => {
-  const data: Uint8Array = await Deno.readFile(img);
-  ImageMagick.read(data, (img: IMagickImage) => {
-    console.log(img.baseWidth);
-  });
+  // const data: Uint8Array = await Deno.readFile(img);
+  // ImageMagick.read(data, (img: IMagickImage) => {
+  //   console.log(img.baseWidth);
+  // });
   return 0;
-}
+};
+
+const projects = await getProjects();
+console.log(projects);
 
 const readme = `
 # Project Plans
 
 <p align="center">
 ${
-  wireframes.map((img) =>
+  projects.filter((p) => !!p.wireframe).map((p) =>
     "  " + `  
-  <a href="./${getRootFromWireframe(img)}">
-    <img src="${img}" ${getWidth(img)} width="45%"/>
+  <a href="./plans/${p.name}">
+    <img src="${p.wireframe}" width="45%"/>
   </a>`.trim()
   ).join("\n")
 }
 </p>
+
+## Development
+
+### Layout
+
+plans
+└ <name>
+  └ README.md
+  └ model.f3d
+  └ model.stl
+  └ plans.pdf
+  └ images
+    └ wireframe.png
+	└ ...
+
+### Format
+
+\`\`\`bash
+$ deno fmt
+\`\`\`
+
+### Build
+
+\`\`\`bash
+$ deno run --unstable --allow-env --allow-read --allow-write index.ts
+\`\`\`
 
 ## LICENSE
 
